fb5bc026e501e402753c3ff76e8dc734a53e1eba
Grant Hutchison <grant.hutchison@gmail.com>
2020-07-16 20:25:34 +0000

    Initial drop

---
+++ Animal.java
@@ -0,0 +1,14 @@
+class Animal {
+  private String type;
+  private String name;
+  private int age;
+  private double weight;
+
+  Animal( String t, String n, int yearsOld, double w){
+    type = t;
+    name = n;
+    age = yearsOld;
+    weight =w;
+  }
+  
+}
---
+++ Main.java
@@ -0,0 +1,6 @@
+class Main{
+public static void main(String[] args){
+  Animal dog = new Animal("dog", "snoopy",10, 10.5);
+  System.out.println(dog);
+}
+}
